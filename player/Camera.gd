cf44c34cc26db3c6348f4d4c19c09204bf6dfd2e
Narcissa Wright <narcissawright@gmail.com>
2020-08-03 00:24:25 -0700

    autocamera (rotation)

--- player/Camera.gd
+++ player/Camera.gd
@@ -3,7 +3,7 @@ extends Camera
 """ 
 To Do:
 	- autocamera:
-		- auto rotate (when playing is moving left or right to the camera facing dir)
+		- [DONE] auto rotate
 		- peer over ledges
 	* autocamera should not function when free cam is enabled (by moving right stick)
 	
@@ -51,6 +51,7 @@ var default_zoom:float = 3.5
 #		"far": 4.2
 #	}
 
+var autocamera = true
 
 # Pan
 var pan:Vector3
@@ -177,41 +178,27 @@ func pause_controls() -> void:
 		if Input.is_action_pressed("X"): zoom -= 0.1
 		zoom = clamp(zoom, 0.5, 10.0)
 
-
-func _physics_process(_t:float) -> void:
-	update_cam_targets()
-	var pushdir:Vector2 = Game.get_stick_input("right")
+func multiple_targets() -> void:
+	var player_amt:float = screen_edge_detector(Game.player.position)
+	var target_amt:float = screen_edge_detector(zl_target_pos)
 	
-	# Pause Mode / Panning
-	if is_paused:
-		pause_controls()
+	player_amt = min(player_amt, 1.0)
+	target_amt = min(target_amt, 1.0)
+	
+	var interpolate_amt:float = 0.5 - (player_amt / 2.0) + (target_amt / 2.0)
+	var goal_target:Vector3 = Game.player.position.linear_interpolate(zl_target_pos, interpolate_amt)
+	
+	# Find goal_target distance from cam direction:
+	# d is how far to move across the cam vector to reach the nearest position to goal_target
+	var d:float = (goal_target - global_transform.origin).dot(-current_pos)
+	# X is the new position on the cam direction vector line
+	var X:Vector3 = global_transform.origin + -current_pos * d
+	var goal_pan:Vector3 = (goal_target - X)
+	pan = pan.linear_interpolate(goal_pan, zl_pan_lerp_amt)
 
-	# If ZL Targeting an object:
-	elif multiple_targets:
-		var player_amt:float = screen_edge_detector(Game.player.position)
-		var target_amt:float = screen_edge_detector(zl_target_pos)
-		
-		player_amt = min(player_amt, 1.0)
-		target_amt = min(target_amt, 1.0)
-		
-		var interpolate_amt:float = 0.5 - (player_amt / 2.0) + (target_amt / 2.0)
-		var goal_target:Vector3 = Game.player.position.linear_interpolate(zl_target_pos, interpolate_amt)
-		
-		# Find goal_target distance from cam direction:
-		# d is how far to move across the cam vector to reach the nearest position to goal_target
-		var d:float = (goal_target - global_transform.origin).dot(-current_pos)
-		# X is the new position on the cam direction vector line
-		var X:Vector3 = global_transform.origin + -current_pos * d
-		var goal_pan:Vector3 = (goal_target - X)
-		pan = pan.linear_interpolate(goal_pan, zl_pan_lerp_amt)
-		
-	# Not Multiple Targets
-	else:
-		if pan != Vector3.ZERO:
-			pan = pan.linear_interpolate(Vector3.ZERO, zl_pan_lerp_amt)
-			if pan.length_squared() < 0.00001: pan = Vector3.ZERO
-		
-#	if Input.is_action_just_pressed('R3'):
+func zoom_change_unused() -> void:
+	pass
+	#	if Input.is_action_just_pressed('R3'):
 #		match zoom_mode:
 #			"medium": zoom_mode = 'near'
 #			"near": zoom_mode = 'far'
@@ -226,26 +213,49 @@ func _physics_process(_t:float) -> void:
 #	if zoom_amt == 0.0: # 1st person
 #		look_at_from_position(Game.player.position, Game.player.position + Game.player.forwards(), Vector3.UP)
 #	else: # 3rd person
+
+func camera_reset() -> void:
+	var goal_pos = default_pos.rotated(Vector3.UP, Game.player.rotation.y)
+	if current_pos.is_equal_approx(goal_pos) or cam_reset_frame >= cam_reset_time:
+		resetting = false
+		autocamera = true
+		cam_reset_frame = 0.0
+		update_position(current_pos)
+	else:
+		cam_reset_frame += 1.0
+		# new_y is the final y position in a normalized vec3
+		var new_y = lerp(current_pos.y, goal_pos.y, cam_reset_frame / cam_reset_time)
+		var xz = Vector2(current_pos.x, current_pos.z).normalized()
+		var xz_goal = Vector2(goal_pos.x, goal_pos.z).normalized()
+		xz = xz.slerp(xz_goal, cam_reset_frame / cam_reset_time) # horizontal rotation
+		# need to multiply the xz values by a multiplier so new_pos is unit length
+		xz *= sqrt(1 - new_y * new_y) # Thanks Syn and Eta
+		var new_pos = Vector3(xz.x, new_y, xz.y) # should be ~unit length
+		update_position(new_pos)
+
+func _physics_process(_t:float) -> void:
+	update_cam_targets()
+	#var pushdir:Vector2 = Game.get_stick_input("right")
+	
+	# Pause Mode / Panning
+	if is_paused:
+		pause_controls()
+	# If ZL Targeting an object:
+	elif multiple_targets:
+		multiple_targets()
+	# Not Multiple Targets
+	else:
+		if pan != Vector3.ZERO:
+			pan = pan.linear_interpolate(Vector3.ZERO, zl_pan_lerp_amt)
+			if pan.length_squared() < 0.00001: pan = Vector3.ZERO
 	
 	if resetting:
-		var goal_pos = default_pos.rotated(Vector3.UP, Game.player.rotation.y)
-		if current_pos.is_equal_approx(goal_pos) or cam_reset_frame >= cam_reset_time:
-			resetting = false
-			cam_reset_frame = 0.0
-			update_position(current_pos)
-		else:
-			cam_reset_frame += 1.0
-			# new_y is the final y position in a normalized vec3
-			var new_y = lerp(current_pos.y, goal_pos.y, cam_reset_frame / cam_reset_time)
-			var xz = Vector2(current_pos.x, current_pos.z).normalized()
-			var xz_goal = Vector2(goal_pos.x, goal_pos.z).normalized()
-			xz = xz.slerp(xz_goal, cam_reset_frame / cam_reset_time) # horizontal rotation
-			# need to multiply the xz values by a multiplier so new_pos is unit length
-			xz *= sqrt(1 - new_y * new_y) # Thanks Syn and Eta
-			var new_pos = Vector3(xz.x, new_y, xz.y) # should be ~unit length
-			update_position(new_pos)
+		camera_reset()
+		return
 	
-	elif pushdir.length_squared() > 0.0:
+	var rightstick = Game.get_stick_input("right")
+	if rightstick.length_squared() > 0.0:
+		autocamera = false
 		var new_pos = current_pos
 		var cross:Vector3 = new_pos.cross(Vector3.UP).normalized()
 		
@@ -256,15 +266,25 @@ func _physics_process(_t:float) -> void:
 			resetting = true
 			
 		if cross != Vector3.ZERO:
-			new_pos = new_pos.rotated(Vector3.UP, -pushdir.x * move_speed)
-			if (pushdir.y > 0.0 and new_pos.y > 0.0) or (pushdir.y < 0.0 and new_pos.y < 0.0):
-				pushdir.y *= 1.0 - abs(new_pos.y)
-			new_pos = new_pos.rotated(cross, pushdir.y * move_speed)
+			new_pos = new_pos.rotated(Vector3.UP, -rightstick.x * move_speed)
+			if (rightstick.y > 0.0 and new_pos.y > 0.0) or (rightstick.y < 0.0 and new_pos.y < 0.0):
+				rightstick.y *= 1.0 - abs(new_pos.y)
+			new_pos = new_pos.rotated(cross, rightstick.y * move_speed)
 			new_pos.y = clamp(new_pos.y, -0.85, 0.85)
 			update_position(new_pos)
+		return
 	
-	else:
-		update_position(current_pos)
+	if autocamera:
+		if Game.player.grounded:
+			var leftstick = Game.get_stick_input("left")
+			var new_pos = current_pos
+			var factor = max(abs(leftstick.x) - 0.25, 0.0) * 0.5
+			var rotation_amount = -leftstick.x * move_speed * factor
+			new_pos = new_pos.rotated(Vector3.UP, rotation_amount)
+			update_position(new_pos)
+			return
+	
+	update_position(current_pos)
 
 func debug() -> void:
 	# Write debug info
