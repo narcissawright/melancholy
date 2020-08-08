9951af1951a7e6907fd4c24739da2f28ab00bc8c
Narcissa Wright <narcissawright@gmail.com>
2020-08-08 11:50:23 -0700

    powerup container

--- global/debug/Debug.gd
+++ global/debug/Debug.gd
@@ -1,4 +1,8 @@
-extends Node
+extends Control
 
 onready var text = $"Text"
 onready var draw = $"Draw"
+
+func _process(delta:float) -> void:
+	if Input.is_action_just_pressed("debug"):
+		visible = not visible
