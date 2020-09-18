4e832fc1cdfb7eb54de7266f3ba606f3cd448b2c
Narcissa Wright <narcissawright@gmail.com>
2020-09-18 00:06:22 -0700

    improved hands

--- actor/checkpoint/Checkpoint.gd
+++ actor/checkpoint/Checkpoint.gd
@@ -1,5 +1,5 @@
 extends MeshInstance
 
 func _on_CheckpointArea_body_entered(body: Node) -> void:
-	if body == Game.player:
+	if body == Player.kinematicbody:
 		Events.emit_signal("checkpoint", global_transform.origin)
