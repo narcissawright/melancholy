bd8106423e4366926a40b874d64a32aeac69536b
Narcissa Wright <narcissawright@gmail.com>
2020-10-22 03:57:57 -0700

    better mushroomfx, character blend script notes
    I need to remove CoreIK, or find a way to get it to import. I am not sure where the import chokes, but the entire setup feels somewhat unorthodox. unfortunately, some of my animations are using CoreIK now and I might want to redo them without it. Not having it complicates some matters (particularly, inherited rotation for child bones down the chain), I need to find a cleaner solution for working with body twist etc

--- ui/ScreenColorFX.gd
+++ ui/ScreenColorFX.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 	Events.connect("mysterious_mushroom", self, "mysterious_mushroom")
 	
 func mysterious_mushroom() -> void:
-	material.set_shader_param("fx_color", Color(0.17,0.71,0.25,0.75))
+	material.set_shader_param("fx_color", Color(0.17,0.71,0.3,0.65))
 	$Anim.play("fade")
