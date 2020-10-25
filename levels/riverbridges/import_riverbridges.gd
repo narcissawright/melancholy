c917bd10bc12d3a313c54ca880e94ff8ce27e99c
Narcissa Wright <narcissawright@gmail.com>
2020-10-25 01:00:43 -0700

    elevator model

--- levels/riverbridges/import_riverbridges.gd
+++ levels/riverbridges/import_riverbridges.gd
@@ -4,11 +4,10 @@ extends EditorScenePostImport
 func post_import(scene):
 	for child in scene.get_children():
 		if child.name == "RiverBridges":
-			child.mesh.set("surface_2/name", "Grass")
 			child.mesh.surface_set_material(1, load("res://levels/riverbridges/grass_shader.tres"))
 			child.set_script(load("res://levels/riverbridges/signal_grass_material.gd"))
-			child.create_trimesh_collision()
-		if child.name == "BowPlats":
-			child.create_trimesh_collision()
+		if child is MeshInstance:
+			if child.name != "Water":
+				child.create_trimesh_collision()
 	
 	return scene
